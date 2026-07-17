921081fdd7066af864a3de1cf83bb78ac168d52a
Ayan Nawaz <ayaankhan809253@gmail.com>
2026-07-17 12:38:20 +0530

    Add C programs for array input, max element, function arrays, even/odd sum, and pair operations

---
+++ oddevensum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+
+    int sumeven = 0;
+    int sumodd = 0;
+
+    for (int i = 0; i < 7; i++) {
+        if (i % 2 == 0) {
+            sumeven += arr[i];
+        } else {
+            sumodd += arr[i];
+        }
+    }
+
+    int res = sumeven - sumodd;
+
+    printf("Sum of even index elements = %d\n", sumeven);
+    printf("Sum of odd index elements = %d\n", sumodd);
+    printf("Difference = %d\n", res);
+
+    return 0;
+}
